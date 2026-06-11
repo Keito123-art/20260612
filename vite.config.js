a1d85f16ed9b5dd187548c3bf498e55a3bf7de44
Keito123-art <angelhsieh80112@gmail.com>
2026-06-12 00:53:43 +0800

    feat: 真正完成自動部署設定

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  base: '/20260612/',
+  
   plugins: [
+    
     vue(),
     vueDevTools(),
   ],
